0395fb43e8c72ea46ca4a841636da85009a3d207
中村　迅 <shu.kyu0224@icloud.com>
2021-11-10 02:59:03 +0900

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      authDomain: "fir-site-d2145.firebaseapp.com",
+      projectId: "fir-site-d2145",
+      storageBucket: "fir-site-d2145.appspot.com",
+      messagingSenderId: "1023233617878",
+      appId: "1:1023233617878:web:ea8009de4530a855dc3e77",
+      measurementId: "G-N20FQD5CNZ"
+    }
+  )
+}
+  
+export default firebase
